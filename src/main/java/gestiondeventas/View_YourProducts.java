a01a3f9e630466ae23aad3a0b83d669c287ce339
Daniil Kobzar <dk@Ordenador-portatil-de-Daniil.local>
2023-04-03 02:50:16 +0200

    .gitignore updated

--- src/main/java/gestiondeventas/View_YourProducts.java
+++ src/main/java/gestiondeventas/View_YourProducts.java
@@ -279,34 +279,6 @@ public class View_YourProducts implements ActionListener{
         }
     }
 
-    public static class CustomMultiLineTableCellRenderer extends JTextArea implements TableCellRenderer {
-        private List<Products> products;
-    
-        public CustomMultiLineTableCellRenderer(List<Products> products) {
-            this.products = products;
-            setLineWrap(true);
-            setWrapStyleWord(true);
-        }
-    
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-            setText((String) value);
-            setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
-            if (table.getRowHeight(row) != getPreferredSize().height) {
-                table.setRowHeight(row, getPreferredSize().height);
-            }
-    
-            Products product = products.get(row);
-            if (product.getStock() <= product.getMin_stock_alert()) {
-                setBackground(Color.RED);
-            } else {
-                setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
-            }
-    
-            setForeground(isSelected ? table.getSelectionForeground() : table.getForeground());
-    
-            return this;
-        }
-    }
     
 
     @Override
